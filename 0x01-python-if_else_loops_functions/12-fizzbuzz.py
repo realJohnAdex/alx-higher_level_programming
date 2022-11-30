39b8305b62832e46d10ea2d7ba3fb0d4931a9561
realJohnAdex <johnadex60@gmail.com>
2022-11-29 22:07:40 -0800

    Update task 12 file

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,7 +4,7 @@ def fizzbuzz():
     For multiples of three print Fizz instead of the number
     and for multiples of five print Buzz
     For numbers which are multiples of both three and five print FizzBuzz."""
-    for n in range(1,101):
+    for n in range(1, 101):
         if (n % 15) == 0:
             print("FizzBuzz", end=' ')
         elif (n % 3) == 0:

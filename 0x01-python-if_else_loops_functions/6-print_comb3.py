e2dec39757538c71108cfc78c0fac73e2b911701
realJohnAdex <johnadex60@gmail.com>
2022-11-29 20:53:03 -0800

    Update task 6 file

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 for i in range(10):
-    for j in range(1, 10):
+    for j in range((1 + i), 10):
         print("{:d}".format(i), end='')
-        print("{:d}".format((i + j) % 10), end=', ')
+        if (i == 8 and j == 9):
+            print("{:d}".format(j))
+        else:
+            print("{:d}".format(j), end=', ')

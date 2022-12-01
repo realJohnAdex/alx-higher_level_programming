035e61b900b98ceed50e573650dcbcef3789fdd7
realJohnAdex <johnadex60@gmail.com>
2022-12-01 13:29:56 -0800

    Update task 0 file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
-from add_0 import add
-a = 1
-b = 2
-res = add(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, res))
+if __name__ == "__main__":
+    """Print the sum of 1 and 2 using add function"""
+    from add_0 import add
+    
+    a = 1
+    b = 2
+    res = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, res))

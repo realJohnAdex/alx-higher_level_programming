2f2237bb31c7be2c32672cf9a47688e7647c9564
realJohnAdex <johnadex60@gmail.com>
2022-12-01 18:06:17 -0800

    Add task 1 file

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """imports functions from the file calculator_1.py,
+        does some Maths, and prints the result."""
+
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))

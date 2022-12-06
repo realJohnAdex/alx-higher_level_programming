0a0ef4a3a6cc10291975d4fbac6d471aabfe7e57
realJohnAdex <johnadex60@gmail.com>
2022-12-05 18:59:29 -0800

    Update task 0 file 1

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    [print("{}".format(item)) for item in my_list if int(item) == item]
+    [print("{}".format(item)) for item in my_list if type(item)is int]

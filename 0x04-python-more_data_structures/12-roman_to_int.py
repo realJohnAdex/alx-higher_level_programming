db22cc863eed606f96af6d2f4f9ed8a6588301bc
realJohnAdex <johnadex60@gmail.com>
2022-12-07 12:19:49 -0800

    Update task 12 file

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
+    if not isinstance(roman_string, str):
+        return 0
+    if roman_string is None:
         return 0
     int_val = 0
     for ch in roman_string:

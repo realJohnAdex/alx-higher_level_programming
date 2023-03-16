6abaf896ba9accd8eaf24e5829c1f6527b4c8af2
realJohnAdex <johnadex60@gmail.com>
2023-03-16 08:26:10 +0100

    Add task 7 file

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,7 +2,7 @@
 def update_dictionary(a_dictionary, key, value):
     for k in list(a_dictionary):
         if k == key:
-            a_dictionary[k] = value
+            a_dictionary.update({k: value})
         else:
             a_dictionary[key] = value
     return a_dictionary

1d9f5bc2bcef939016fcee4cdd92f503572dcb8e
realJohnAdex <johnadex60@gmail.com>
2023-03-16 17:46:54 +0100

    Add task 13 file

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    total_weighted_score = 0
+    total_weight = 0
+    for (score, weight) in my_list:
+        total_weighted_score += (score * weight)
+        total_weight += weight
+    result = total_weighted_score / total_weight
+    return result

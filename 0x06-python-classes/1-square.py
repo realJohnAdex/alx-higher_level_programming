29e08aba1d45619c8d2fca0f27a8d67db85f140c
realJohnAdex <johnadex60@gmail.com>
2023-03-29 22:17:22 +0100

    Add task 1 file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This is a module defining a new object Square"""
+
+
+class Square:
+    """A simple imnplementation of a Square"""
+    def __init__(self, size):
+        self.__size = size

c17a474a4a65f7edf1c772b30e2495e908cbbf37
realJohnAdex <johnadex60@gmail.com>
2023-03-30 11:06:57 +0100

    Make private var init

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,8 +5,8 @@
 class Square:
     """A simple imnplementation of a Square"""
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):

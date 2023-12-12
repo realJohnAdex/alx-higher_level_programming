57d6bc5a3929d08442a3346cea92aa5935699f7a
realJohnAdex <johnadex60@gmail.com>
2023-12-12 04:52:16 -0800

    Update

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,7 @@
+#!/usr/bin/node
 // Create a constant variable called myVar with the value "JavaScript is amazing"
 const myVar = 'JavaScript is amazing';
 
 // Use console.log(...) to print the value of myVar
 console.log(myVar);
+

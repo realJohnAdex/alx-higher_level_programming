f04e632bbb9e8b364213919c4d285a485fa53085
realJohnAdex <johnadex60@gmail.com>
2023-12-12 13:34:29 -0800

    Add task 10 file'

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// Function to compute factorial recursively
+const factorial = (n) => {
+  if (isNaN(n)) {
+    return 1;
+  }
+
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+};
+
+// Get the arguments
+const myInt = parseInt(process.argv[2]);
+
+// Printing outputs
+console.log(factorial(myInt));

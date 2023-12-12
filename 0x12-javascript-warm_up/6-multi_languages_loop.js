906e1cb1594461f2356c0a4b2209d09265e2b32d
realJohnAdex <johnadex60@gmail.com>
2023-12-12 07:08:50 -0800

    Ad task 6 file

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Create an array of strings
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// Use console.log(...) to print the value of myVar
+for (let i = 0; i < myArray.length; i++) {
+  console.log(myArray[i]);
+}

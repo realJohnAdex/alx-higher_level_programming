8c9359de9e4ca335db34e1faa5de606e6e402855
realJohnAdex <johnadex60@gmail.com>
2023-12-12 05:06:14 -0800

    Add task 1 file

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Create a constant variable called myVar with the value "JavaScript is amazing"
+const myVar3 = 'JavaScript is amazing';
+const myVar2 = 'Python is cool';
+const myVar1 = 'C is fun';
+
+// Use console.log(...) to print the value of myVar
+console.log(myVar1);
+console.log(myVar2);
+console.log(myVar3);

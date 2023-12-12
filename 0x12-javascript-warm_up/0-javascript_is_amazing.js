afdb455f4a2956cfac6efaf40ee84a5fb094e494
realJohnAdex <johnadex60@gmail.com>
2023-12-12 04:46:11 -0800

    Add task 0 file

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,5 @@
+// Create a constant variable called myVar with the value "JavaScript is amazing"
+const myVar = "JavaScript is amazing";
+
+// Use console.log(...) to print the value of myVar
+console.log(myVar);

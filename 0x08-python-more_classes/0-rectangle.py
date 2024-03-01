25494c31a5b872bab4f19cbcd5e546f2067294d7
realjohnadex <johnadex60@gmail.com>
2024-03-01 09:51:52 -0800

    Add task 0 file

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+class Rectangle:
+    """
+    Represents a rectangle.
+
+    Attributes:
+        None
+    """
+
+    def __init__(self):
+        """
+        Initializes a new instance of the Rectangle class.
+        """
+        pass

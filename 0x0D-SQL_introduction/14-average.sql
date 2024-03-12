7123c4c78ed52746d054ddd3bc39de5968c74aa5
realjohnadex <johnadex60@gmail.com>
2024-03-12 14:11:17 -0700

    Add task 14 file

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- lists all rows of the table first_table from the db hbtn_0c_0
+-- All fields should be printed
+-- db name will be passed as an argument of the mysql command
+SELECT AVG(score) AS average FROM second_table;

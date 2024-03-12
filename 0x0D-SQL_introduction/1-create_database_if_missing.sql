74656f6007b82733b5f49664c699eb04e49a15b8
realjohnadex <johnadex60@gmail.com>
2024-03-12 09:06:43 -0700

    Add task  file

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- creates the database hbtn_0c_0 in your MySQL server
+-- If the database already exists, the script will not fail
+-- SELECT or SHOW statements not allowed
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;

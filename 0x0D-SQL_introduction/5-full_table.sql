9f387e04b232ad91f3e6d38e06e53439fbb86365
realjohnadex <johnadex60@gmail.com>
2024-03-12 10:15:01 -0700

    Add task 5 file

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- prints the full description first_table from the db hbtn_0c_0
+-- database name will be passed as an argument of the mysql
+-- DESCRIBE or EXPLAIN statements not allowed
+SHOW CREATE TABLE first_table

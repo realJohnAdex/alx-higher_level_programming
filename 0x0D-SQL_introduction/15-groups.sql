a058ee78a38601154814cfcbb965ea71b4b35b59
realjohnadex <johnadex60@gmail.com>
2024-03-12 15:09:56 -0700

    Update task 15 file

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
 -- lists all rows of the table first_table from the db hbtn_0c_0
 -- All fields should be printed
--- db name will be passed as an argument of the mysql command
-SELECT score, COUNT(score) AS number FROM second_table  GROUP BY score;
+-- db name will be passed as an argument of the mysql commandi
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;

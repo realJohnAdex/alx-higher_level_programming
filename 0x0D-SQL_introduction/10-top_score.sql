60d0cd07e7d195c2362de90c586de3bda0ddd479
realjohnadex <johnadex60@gmail.com>
2024-03-12 13:27:36 -0700

    Add task 10 file

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- lists all rows of the table first_table from the db hbtn_0c_0
+-- All fields should be printed
+-- db name will be passed as an argument of the mysql command
+SELECT score, name FROM second_table ORDER BY score DESC;

4b08f44ee2aab9da26b13ca86d4c64d3fc5092ec
realjohnadex <johnadex60@gmail.com>
2024-03-12 13:38:13 -0700

    Add task 12 file

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- lists all rows of the table first_table from the db hbtn_0c_0
+-- All fields should be printed
+-- db name will be passed as an argument of the mysql command
+UPDATE second_table SET score = 10 WHERE name = "Bob";

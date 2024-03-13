deb83a92d479fcf73cb86d07ec8bbc0ea0f08e9b
realjohnadex <johnadex60@gmail.com>
2024-03-12 17:51:18 -0700

    Add task 19 file

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,8 @@
+-- displays the average temperature (Fahrenheit)
+-- by city ordered by temperature (descending).
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE month IN (7, 8)
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;

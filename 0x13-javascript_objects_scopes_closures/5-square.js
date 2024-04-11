887eeaf27c29bb0cd92469931a70bec3acbca695
Yinka ☯️ <johnadex60@gmail.com>
2024-04-11 04:41:33 +0000

    Refactor Square class to fix module export and import Rectangle class in 0x13-javascript_objects_scopes_closures/5-square.js

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,7 +1,7 @@
 #!usr/bin/node
 const Rectangle = require('./4-rectangle');
 
-class Square extends Rectangle {
+module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }

adf663d73b2600d8d09658427a5ece33c130bcb6
Yinka ☯️ <johnadex60@gmail.com>
2024-04-11 04:20:12 +0000

    Add Rectangle class implementation in 0x13-javascript_objects_scopes_closures/3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+    print () {
+        let str = '';
+        for (let i = 0; i < this.width; i++) {
+        str += 'X';
+        }
+        for (let i = 0; i < this.height; i++) {
+        console.log(str);
+        }
+    }
+};

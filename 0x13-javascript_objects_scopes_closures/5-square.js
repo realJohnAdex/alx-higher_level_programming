f078e013e9529a4b88517e8afc751a38dfbf3809
Yinka ☯️ <johnadex60@gmail.com>
2024-04-11 04:43:38 +0000

    Fix module export and import in 0x13-javascript_objects_scopes_closures/5-square.js

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,7 +1,5 @@
-#!usr/bin/node
-const Rectangle = require('./4-rectangle');
-
-module.exports = class Square extends Rectangle {
+#!/usr/bin/node
+module.export = class Square extends require('./4-rectangle') {
   constructor (size) {
     super(size, size);
   }

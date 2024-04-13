a92bcc0a89ffbfa7bcee4b6c142bd37362e0c7c6
realjohnadex <johnadex60@gmail.com>
2024-04-13 02:05:24 -0700

    Add virtual environment setup files

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+def list_states(username, password, database):
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+
+    # Create cursor
+    cursor = db.cursor()
+
+    # Execute query to select all states
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all rows
+    rows = cursor.fetchall()
+
+    # Print results
+    for row in rows:
+        print(row)
+
+    # Close cursor and connection
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    # Get arguments
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    # Call function to list states
+    list_states(username, password, database)
--- 0x0F-python-object_relational_mapping/README.md
+++ 0x0F-python-object_relational_mapping/README.md
@@ -16,7 +16,8 @@ query_rows = cur.fetchall()
 for row in query_rows:
     print(row)
 cur.close()
-conn.close()```
+conn.close()
+```
 With an ORM:
 ```
 engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format("root", "root", "my_db"), pool_pre_ping=True)
@@ -25,5 +26,6 @@ Base.metadata.create_all(engine)
 session = Session(engine)
 for state in session.query(State).order_by(State.id).all(): # HERE: no SQL query, only objects!
     print("{}: {}".format(state.id, state.name))
-session.close()```
+session.close() 
+```
 Do you see the difference? Cool, right?
